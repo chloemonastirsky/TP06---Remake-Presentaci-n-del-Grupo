8e592732f4ad8b8318024517fc36f33afdab3c7c
CATALINA COHEN <49092736@ort.edu.ar>
2025-07-15 12:11:19 -0300

    15-7

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,11 +10,12 @@ public class HomeController : Controller
 
     public HomeController(ILogger<HomeController> logger)
     {
-        _logger = logger;
+        
     }
 
     public IActionResult Index()
     {
+        
         return View();
     }
 }
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,4 +1,6 @@
-using Microsoft.Data.SqlClient;
+
+namespace TP06;
+using System.Data.SqlClient;
 using Dapper;
 
 public static class BD{
@@ -6,7 +8,7 @@ public static class BD{
 
     public static int Login(string email, string contraseña){
         int encontrado=0;
-        using(SqlConnection connection = new SqlConnection){
+        using(SqlConnection connection = new SqlConnection(conexion)){
             string query = "select Contraseña from Usuario where email="+email;
             encontrado= connection.QueryFirstOrDefault<int>(query, new {pContraseña=contraseña});
 
@@ -22,5 +24,35 @@ public static class BD{
     public static Usuario GetUsuario(int id){
         Usuario usuarioMostrar=null;
 
+        using(SqlConnection connection = new SqlConnection(conexion)){
+            string query = "select Usuario.Id, Usuario.Nombre, Usuario.Apellido, Usuario.Email, Usuario.Contraseña, Usuario.FechaNacimiento, Usuario.Foto from Usuario where id="+ id;
+            usuarioMostrar= connection.QueryFirstOrDefault<Usuario>(query, new {pid=id});
+        }
+
+        return usuarioMostrar;
+
+    }
+
+    public static List<DatoFamiliar> GetDatoFamiliar(int id) {
+
+        List<DatoFamiliar> LDatosFliares=null;
+        using(SqlConnection connection = new SqlConnection(conexion)){
+            string query = "select DatoFamiliar.IdUsuario, DatoFamiliar.Nombre, DatoFamiliar.Apellido, DatoFamiliar.Parentesco, DatoFamiliar.Descripcion from DatoFamiliar where id="+ id;
+            LDatosFliares= connection.Query<DatoFamiliar>(query).ToList();
+        }
+
+        return LDatosFliares;
+
+    }
+
+    public static  List<DatoInteres> GetDatoInteres(int id){
+        
+        List<DatoInteres> LDatoInteres=null;
+        using(SqlConnection connection = new SqlConnection(conexion)){
+            string query = "select  DatoInteres.Id, DatoInteres.IdUsuario, DatoInteres.TipoInteres, DatoInteres.Interes from DatoInteres where id="+ id;
+            LDatoInteres= connection.Query<DatoInteres>(query).ToList();
+        }
+
+        return LDatoInteres;
     }
 }
